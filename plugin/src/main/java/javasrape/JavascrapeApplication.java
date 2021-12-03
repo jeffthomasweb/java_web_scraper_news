0a1612dcd8a117c5c7fb099375dd2eec107b3cb0
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2021-12-03 16:35:21 -0500

    Update JavascrapeApplication.java

--- plugin/src/main/java/javasrape/JavascrapeApplication.java
+++ plugin/src/main/java/javasrape/JavascrapeApplication.java
@@ -34,47 +34,17 @@ public class JavascrapeApplication {
   			String linkText = link.text();
   			results.add(linkText);
 			}
-	 	String a = results.get(1);
-	 	String b = results.get(2);
-	 	String c = results.get(3);
-	 	String d = results.get(4);
-	 	String e = results.get(5);
-	 	String f = results.get(6);
-	 	String g = results.get(7);
-	 	String h = results.get(8);
-	 	String i = results.get(9);
-	 	String j = results.get(10);
-	 	String k = results.get(11);
-	 	String l = results.get(12);
-	 	String m = results.get(13);
-	 	String n = results.get(14);
-	 	String o = results.get(15);
-	 	String p = results.get(16);
-	 	String q = results.get(17);
-	 	String r = results.get(18);
-	 	String s = results.get(19);
-		String t = results.get(20);
+	 
 		List<String> resultsf = new ArrayList<String>();
+		
+		for (String individualLinkText: results) {
+			resultsf.add(individualLinkText);
+		}
 
-	 	resultsf.add(b);
-	 	resultsf.add(c);
-	 	resultsf.add(d);
-	 	resultsf.add(e);
-	 	resultsf.add(f);
-	 	resultsf.add(g);
-	 	resultsf.add(h);
-	 	resultsf.add(i);
-	 	resultsf.add(j);
-	 	resultsf.add(k);
-	 	resultsf.add(l);
-	 	resultsf.add(m);
-	 	resultsf.add(n);
-	 	resultsf.add(o);
-	 	resultsf.add(p);
-	 	resultsf.add(q);
-	 	resultsf.add(r);
-		resultsf.add(s);
-		resultsf.add(t);
-	return resultsf;
+    
+		List<String> partResultsf = new ArrayList<String>();
+		partResultsf = resultsf.subList(1,21);
+	
+	return partResultsf;
 }
 }
